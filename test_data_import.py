39c00c772429bc86b93e155e3820398a9aa7339a
Ryan Morelock <RyanMorelock@cu-biotgw-4-10.219.134.152.int.colorado.edu>
2019-10-14 09:03:28 -0600

    linear and binary search tests for times in and not in time array

--- test_data_import.py
+++ test_data_import.py
@@ -6,9 +6,40 @@ import data_import
 
 class TestDataImport(unittest.TestCase):
     def test_import_data(self):
-        filename = './smallData/activity_small.csv'
+        filename = './smallData/smbg_small.csv'
         obj = data_import.ImportData(filename)
         self.assertEqual(len(obj._time), len(obj._value))
+
+    def test_linear_search_true(self):
+        filename = './smallData/smbg_small.csv'
+        obj = data_import.ImportData(filename)
+        d = datetime.datetime(2018, 3, 16, 8, 46)
+        index = obj.linear_search_value(d)
+        self.assertEqual(index, 0)
+
+    def test_linear_search_false(self):
+        filename = './smallData/smbg_small.csv'
+        obj = data_import.ImportData(filename)
+        d = datetime.datetime(2018, 2, 16, 8, 46)
+        index = obj.linear_search_value(d)
+        self.assertEqual(index, -1)
+
+    def test_binary_search_true(self):
+        filename = './smallData/smbg_small.csv'
+        obj = data_import.ImportData(filename)
+        obj.binary_sort()
+        d = datetime.datetime(2018, 3, 16, 8, 46)
+        index = obj.binary_search_value(d)
+        self.assertEqual(index, 0)
+
+    def test_binary_search_false(self):
+        filename = './smallData/smbg_small.csv'
+        obj = data_import.ImportData(filename)
+        obj.binary_sort()
+        d = datetime.datetime(2018, 2, 16, 8, 46)
+        index = obj.binary_search_value(d)
+        self.assertEqual(index, -1)
+
 
 
 if __name__ == '__main__':
